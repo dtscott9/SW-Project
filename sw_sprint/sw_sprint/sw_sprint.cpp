84642b15b89bd8ee5081a61bbf1d99840e77b4e8
dtscott9 <dtscott9@byui.edu>
2022-11-17 12:47:34 -0700

    added jsoncpp

--- sw_sprint/sw_sprint/sw_sprint.cpp
+++ sw_sprint/sw_sprint/sw_sprint.cpp
@@ -1,20 +1,26 @@
 // sw_sprint.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <cstdlib>
 #include <iostream>
+#include <fstream>
+#include <json/json.h>
+#include <json/value.h>
+using namespace std;
+using namespace Json;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    /*cout << "Hello World!\n";*/
+    Reader reader;
+    Value value;
+
+    ifstream file("character.json");
+
+    reader.parse(file, value);
+
+    const Value newValue = value["name"];
+
+    cout <<"Value is: \n" << value;
+    
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
